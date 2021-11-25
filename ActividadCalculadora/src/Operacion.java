e2788c1430ccab92885f1df0c75bf3c89c8d01ce
Iván Cetina <95014808+MoonHyperion@users.noreply.github.com>
2021-11-25 04:57:05 -0600

    Add files via upload

---
+++ ActividadCalculadora/src/Operacion.java
@@ -0,0 +1,29 @@
+public class Operacion {
+    
+    double n1;
+    double n2;
+    double res;
+    char operacion;
+    int n;
+
+    
+    public Operacion(double n1, double n2, char operacion) {
+        
+        this.n1 = n1;
+        this.n2 = n2;
+        this.operacion = operacion;
+    }
+    
+    
+    
+        
+    public void mostrarResultado(){
+        
+        System.out.println(this.n1+" "+this.operacion+" "+this.n2+" = "+this.res);
+        
+    }
+    
+    public void setRes(double res){
+        this.res=res;
+    }
+}
